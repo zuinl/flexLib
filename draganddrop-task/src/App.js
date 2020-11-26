3baaf4b1140ead27b065ada3046e759d31e50065
Leonardo <leonardosoareszuin@gmail.com>
2020-11-26 10:25:11 -0300

    first commit

---
+++ draganddrop-task/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd'
+import Task from './Task'
+
+let toDo = [{
+  'id': '1',
+  'title': 'Grocery',
+  'list': ['apple', 'banana', 'milk', 'eggs', 'bread']
+}]
+
+let onDragEnd = result => {
+  console.log(result)
+  if (!result.destination) {
+    return;
+  }
+  let sourceIdx = parseInt(result.source.index)
+  let destIdx = parseInt(result.destination.index)
+  let draggedLink = toDo[0].list[sourceIdx]
+  let newList = toDo[0].list.slice();
+  newList.splice(sourceIdx, 1);
+  newList.splice(destIdx, 0, draggedLink)
+  console.log(newList)
+  toDo[0].list = newList;
+}
+
+function App() {
+  return (
+    <DragDropContext onDragEnd={onDragEnd} >
+      <Task toDo={toDo[0]} />
+    </DragDropContext>
+  );
+}
+export default App;
